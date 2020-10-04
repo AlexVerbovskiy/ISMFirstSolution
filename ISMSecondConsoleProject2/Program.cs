6c0ff7040b5836ac58fdbffcf8e76db0e97fe88f
Alexandr <cofeek5@email.com>
2020-10-04 14:03:43 +0300

    Створення Second Project

---
+++ ISMSecondConsoleProject2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ISMSecondConsoleProject2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Second Project");
+        }
+    }
+}
